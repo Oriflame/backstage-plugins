b9ff1b3f395e5518b6780cea92323a2f35d365e5
Jan Vilimek <jvilimek@users.noreply.github.com>
2022-09-19 16:45:01 +0200

    healthcheck

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -7,6 +7,8 @@
  */
 
 // eslint-disable-next-line notice/notice
+const { version } = require('../package.json');
+
 import Router from 'express-promise-router';
 import {
   CacheManager,
@@ -21,6 +23,7 @@ import {
   ServerTokenManager,
 } from '@backstage/backend-common';
 import { Config } from '@backstage/config';
+import healthcheck from './plugins/healthcheck';
 import { TaskScheduler } from '@backstage/backend-tasks';
 import app from './plugins/app';
 import auth from './plugins/auth';
@@ -30,21 +33,38 @@ import proxy from './plugins/proxy';
 import { PluginEnvironment } from './types';
 import { ServerPermissionClient } from '@backstage/plugin-permission-node';
 
+const safeFatalError = async (message: string, error?: Error) => {
+  try {
+    // if available, use the root logger (shall send the message to AppInsights too)
+    const logger = getRootLogger();
+    logger.error(message);
+  } catch (_e) {
+    console.error(message);
+  }
+  if (process.env.NODE_ENV === 'development') {
+    console.error(
+      `DEVELOPMENT server (watcher) ${process.ppid} will be killed now...`,
+    );
+    process.kill(process.ppid);
+  }
+  console.info(`Process ${process.pid} will exit now...`);
+  process.exit(1);
+};
+
 function makeCreateEnv(config: Config) {
   const root = getRootLogger();
   const reader = UrlReaders.default({ logger: root, config });
   const discovery = SingleHostDiscovery.fromConfig(config);
-  const taskScheduler = TaskScheduler.fromConfig(config);
-
-  root.info(`Created UrlReader ${reader}`);
-
-  const databaseManager = DatabaseManager.fromConfig(config);
-  const cacheManager = CacheManager.fromConfig(config);
   const tokenManager = ServerTokenManager.fromConfig(config, { logger: root });
   const permissions = ServerPermissionClient.fromConfig(config, {
     discovery,
     tokenManager,
   });
+  const databaseManager = DatabaseManager.fromConfig(config);
+  const cacheManager = CacheManager.fromConfig(config);
+  const taskScheduler = TaskScheduler.fromConfig(config);
+
+  root.info(`Created UrlReader ${reader}`);
 
   return (plugin: string): PluginEnvironment => {
     const logger = root.child({ type: 'plugin', plugin });
@@ -66,12 +86,17 @@ function makeCreateEnv(config: Config) {
 }
 
 async function main() {
+  const logger = getRootLogger();
+
+  logger.info(`App starting: version [${version}]`);
+
   const config = await loadBackendConfig({
     argv: process.argv,
-    logger: getRootLogger(),
+    logger,
   });
   const createEnv = makeCreateEnv(config);
 
+  const healthcheckEnv = useHotMemoize(module, () => createEnv('healthcheck'));
   const catalogEnv = useHotMemoize(module, () => createEnv('catalog'));
   const scaffolderEnv = useHotMemoize(module, () => createEnv('scaffolder'));
   const authEnv = useHotMemoize(module, () => createEnv('auth'));
@@ -85,19 +110,28 @@ async function main() {
   apiRouter.use('/proxy', await proxy(proxyEnv));
   apiRouter.use(notFoundHandler());
 
-  const service = createServiceBuilder(module)
-    .loadConfig(config)
-    .addRouter('/api', apiRouter)
-    .addRouter('', await app(appEnv));
+  try {
+    logger.info('Creating service...');
+    const service = createServiceBuilder(module)
+      .loadConfig(config)
+      .addRouter('', await healthcheck(healthcheckEnv))
+      .addRouter('/api', apiRouter)
+      .addRouter('', await app(appEnv));
 
-  await service.start().catch(err => {
-    console.log(err);
-    process.exit(1);
-  });
+    logger.info('Starting service...');
+    await service.start().catch(async err => {
+      await safeFatalError(`service.start() failed: ${err}`);
+    });
+  } catch (error) {
+    if (typeof error === 'string') {
+      await safeFatalError(`main() failed: ${error}`);
+    } else if (error instanceof Error) {
+      await safeFatalError(`main() failed: ${error.message}`, error as Error);
+    }
+  }
 }
 
 module.hot?.accept();
-main().catch(error => {
-  console.error(`Backend failed to start up, ${error}`);
-  process.exit(1);
+main().catch(async error => {
+  await safeFatalError(`Backend failed to start up: ${error}`);
 });
